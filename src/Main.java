bf507034fb046996299a4482ff1247360b288056
ngue-237 <ngueemmanuel.prof@gmail.com>
2024-09-04 20:54:37 +0100

    suppression code chaine caractères

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        String nom = "Ngué Tougoum";
-        String prenom = "Emmanuel Benjamin";
-        //le signe + permet de faire la concaténation de deux chaines de
-        //Ngué Tougoum Emmanuel Benjamin
-        String nomComplet = nom + " " + prenom;
-        System.out.println("Le nom complet est : " + nomComplet);
+
     }
 }
